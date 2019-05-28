c1f3cb58d91d9e48be5dc902e9ed95465a85e395
zhuanghua <42395975+lupanlpb@users.noreply.github.com>
2019-05-28 16:19:51 +0800

    upload seismic_source_simulation

---
+++ seismic_source_simulation/seismic_source_simulation/Debug/source_force__genmod.f90
@@ -0,0 +1,12 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Mar 28 20:52:48 2017
+        MODULE SOURCE_FORCE__genmod
+          INTERFACE 
+            SUBROUTINE SOURCE_FORCE(FRE_WAVELET,DELAY_SOURCE_T0,TIME,FC0&
+     &)
+              REAL(KIND=4) :: FRE_WAVELET
+              REAL(KIND=4) :: DELAY_SOURCE_T0
+              REAL(KIND=4) :: TIME
+              REAL(KIND=4) :: FC0
+            END SUBROUTINE SOURCE_FORCE
+          END INTERFACE 
+        END MODULE SOURCE_FORCE__genmod
